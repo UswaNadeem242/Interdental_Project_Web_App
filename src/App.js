1ba779441db647f8ddb82fef37ff1f4b7de651ff
Abad Naseer <77478982+AbadNaseer@users.noreply.github.com>
2025-05-27 18:19:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   return (
     <div>
       <AuthProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/">
           {/* <Header /> */}
           <Routes>
             <Route path="/" element={<LandingPage />} />
