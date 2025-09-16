432be3f99eb9beec6afc97b8ff51746f95ade9ea
uswanadeem123 <uswa.nadeem@poshmaals.com>
2025-09-16 16:28:27 +0500

    fix:rename url

--- src/pages/doctorAdmin/Doctor.jsx
+++ src/pages/doctorAdmin/Doctor.jsx
@@ -12,13 +12,13 @@ const DoctorAdmin = () => {
   return (
     <div className="">
       <Routes>
-        <Route path="/doctor-dashboard" element={<DoctorDashaboard />} />
+        <Route path="/dashboard" element={<DoctorDashaboard />} />
         <Route path="/patient" element={<PatientPage />} />
         <Route path="/claim-request" element={<ClaimRequest />} />
-        <Route path="/orders-details" element={<DoctorOrder />} />
+        <Route path="/place-order" element={<DoctorOrder />} />
         <Route path="/profile" element={<DoctorProfile />} />
         <Route path="/orders" element={<OrderDoctorPage />} />
-        <Route path="/details" element={<DoctorDeailsPage />} />
+        <Route path="/order-details" element={<DoctorDeailsPage />} />
 
 
       </Routes>
