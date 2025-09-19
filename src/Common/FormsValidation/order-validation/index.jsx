d4290d7e08a4ac3a210f2a52acd2ae1ae3a7fcb5
uswanadeem123 <uswa.nadeem@poshmaals.com>
2025-09-19 17:52:14 +0500

    fixed it

--- src/Common/FormsValidation/order-validation/index.jsx
+++ src/Common/FormsValidation/order-validation/index.jsx
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 export const OrderValidationSchema = Yup.object().shape({
     doctorName: Yup.string().required("Doctor's Name / Office Name is required"),
     officeReg: Yup.string().required("Office registration number is required")
-        .matches(/^[0-9]{9}$/, "Office registration number must be exactly 20 digits"),
+        .matches(/^[0-9]{10}$/, "Office registration number must be exactly 10 digits"),
     createDate: Yup.date().required("Create Date is required"),
     dueDate: Yup.date().required("Case expected due date"),
     patientFirstName: Yup.string().required(" name is required"),
