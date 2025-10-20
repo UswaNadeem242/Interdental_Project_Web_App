07028fc7e58c906785822817b5718ac3b807ce5e
Sharjeel <sharjeel.nust07@gmail.com>
2025-10-20 16:38:46 +0500

    About us page fixed

--- src/components/landing-page-component/our-modules.jsx
+++ src/components/landing-page-component/our-modules.jsx
@@ -1,46 +1,46 @@
-import CircleTrickIcon from '../../icon/circle-trick-icon'
-import { ArrowRightIcon } from '@heroicons/react/24/outline'
-import { useNavigate } from 'react-router-dom';
-import { ThirdButtonUI } from '../../Common/Button';
+import CircleTrickIcon from "../../icon/circle-trick-icon";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { useNavigate } from "react-router-dom";
+import { ThirdButtonUI } from "../../Common/Button";
 function OurModules() {
-    const navigate = useNavigate();
-    return (
-        <div className=''>
-            <section className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center md:px-16 px-4 py-20 bg-[#F7FCFC]">
-
-                <div className="space-y-4">
-                    <h1 className="text-sm    md:text-4xl font-bold font-poppins  ">
-                        <span className='text-fouthBrand'> Dental Lab</span>  <span className='text-secondaryBrand'>Alliance (DLA)</span>
-                    </h1>
-                    <p className='text-xl font-normal font-poppins text-primaryText'>Benefit for dentists:</p>
-                    <ul className="flex flex-col space-y-1">
-                        <li className="flex items-start gap-3 bg-white p-3 rounded-md font-poppins font-normal text-sm text-primaryText">
-                            Access to a nationwide lab network.
-                        </li>
-                        <li className="flex items-start gap-3 bg-white p-3 rounded-md font-poppins font-normal text-sm text-primaryText">
-
-                            Freedom to choose labs by quality, cost, or speed.
-                        </li>
-                        <li className="flex items-start gap-3 bg-white p-3 rounded-md font-poppins font-normal text-sm text-primaryText">
-                            Transparent pricing and profiles with no hidden surprises.
-                        </li>
-                        <li className="flex items-start gap-3 bg-white p-3 rounded-md font-poppins font-normal text-sm text-primaryText">
-                            Centralized communication with no more phone calls.
-                        </li>
-                    </ul>
-                    <ThirdButtonUI title=' Explore Labs Now' />
-
-                </div>
-                <div className="flex justify-center">
-                    <img
-                        src="/assets/landing-page/landing 1.png"
-                        alt="Doctor Enrollment"
-                        className="w-full max-w-md"
-                    />
-                </div>
-            </section>
+  const navigate = useNavigate();
+  return (
+    <div className="">
+      <section className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center md:px-16 px-4 py-20 bg-[#F7FCFC]">
+        <div className="space-y-4">
+          <h1 className="text-sm    md:text-4xl font-bold font-poppins  ">
+            <span className="text-fouthBrand"> Dental Lab</span>{" "}
+            <span className="text-secondaryBrand">Alliance (DLA)</span>
+          </h1>
+          <p className="text-xl font-normal font-poppins text-primaryText">
+            Benefit for dentists:
+          </p>
+          <ul className="flex flex-col space-y-1">
+            <li className="flex items-start gap-3  p-3 rounded-md font-poppins font-normal text-sm text-primaryText">
+              Access to a nationwide lab network.
+            </li>
+            <li className="flex items-start gap-3  p-3 rounded-md font-poppins font-normal text-sm text-primaryText">
+              Freedom to choose labs by quality, cost, or speed.
+            </li>
+            <li className="flex items-start gap-3  p-3 rounded-md font-poppins font-normal text-sm text-primaryText">
+              Transparent pricing and profiles with no hidden surprises.
+            </li>
+            <li className="flex items-start gap-3  p-3 rounded-md font-poppins font-normal text-sm text-primaryText">
+              Centralized communication with no more phone calls.
+            </li>
+          </ul>
+          <ThirdButtonUI title=" Explore Labs Now" />
         </div>
-    )
+        <div className="flex justify-center">
+          <img
+            src="/assets/landing-page/landing 1.png"
+            alt="Doctor Enrollment"
+            className="w-full max-w-md"
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default OurModules
+export default OurModules;
--- src/pages/landing-page/warranty.jsx
+++ src/pages/landing-page/warranty.jsx
@@ -2,15 +2,12 @@ import { useState } from "react";
 import { warrantyCards } from "../../Constant";
 import CircleIcon from "../../icon/circle-icon";
 
-
-
 const Warranty = () => {
   return (
     <section className="container px-4 mx-auto py-28  border-b border-background">
       <div className="flex justify-center ">
         <h1 className="text-primaryText text-base md:text-3xl font-normal font-poppins capitalize">
           THE
-
           <span className="md:text-3xl font-bold font-poppins text-secondaryBrand capitalize ml-2">
             WARRANTY
           </span>
@@ -18,7 +15,8 @@ const Warranty = () => {
       </div>
       <div className="flex justify-center pb-6">
         <p className="text-secondaryText text-sm font-poppins text-center max-w-3xl py-6">
-          Interoral.ai provides peace of mind and a unique practice growth opportunity with a comprehensive Warranty Plans and Referal Program
+          Interoral.ai provides peace of mind and a unique practice growth
+          opportunity with a comprehensive Warranty Plans and Referal Programa
         </p>
       </div>
       <div>
@@ -28,14 +26,13 @@ const Warranty = () => {
               key={index}
               className="bg-white shadow-lg rounded-xl overflow-hidden grid grid-cols-1 sm:grid-cols-2"
             >
-
               {/* Left - Image */}
               <div className="w-full h-full">
                 <img
                   src={card.img}
                   alt={card.title}
-                  className="w-full sm:w-[200px] md:w-[290px] lg:w-[300px] h-[150px] sm:h-[180px] md:h-[200px] lg:h-[288px] m-2 sm:m-4 rounded-lg object-cover" />
-
+                  className="w-full sm:w-[200px] md:w-[290px] lg:w-[300px] h-[150px] sm:h-[180px] md:h-[200px] lg:h-[288px] m-2 sm:m-4 rounded-lg object-cover"
+                />
               </div>
 
               {/* Right - Content */}
@@ -69,7 +66,6 @@ const Warranty = () => {
           ))}
         </div>
       </div>
-
     </section>
   );
 };
