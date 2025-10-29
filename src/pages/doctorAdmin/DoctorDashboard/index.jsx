523418f0a50ae8794aa2a50f27402aee8e0ceca3
frivx <frivxd10@gmail.com>
2025-10-29 17:24:53 +0500

    updated

--- src/pages/doctorAdmin/DoctorDashboard/index.jsx
+++ src/pages/doctorAdmin/DoctorDashboard/index.jsx
@@ -127,7 +127,12 @@ const DoctorDashaboard = () => {
             getDoctorStats(),
             getDoctorOrders(doctor_id),
             getGrapgStats("month"),
-            getDoctorPatients(),
+            getDoctorPatients({
+              status: "ALL",
+              page: 0,
+              limit: 100,
+              search: ""
+            }),
           ]);
 
         // Only update state if component is still mounted
@@ -144,6 +149,7 @@ const DoctorDashaboard = () => {
             const transformedOrders = transformOrdersData(
               ordersResponse.data.data
             );
+            console.log(transformedOrders,'transformedOrders');
             setDoctorOrders(transformedOrders);
           } else {
             console.error("Failed to fetch doctor orders:", ordersResponse);
@@ -158,9 +164,9 @@ const DoctorDashaboard = () => {
 
           // Handle patients response
           if (patientsResponse?.status === 200) {
-            const transformedPatients = transformPatientsData(
-              patientsResponse.data.data?.data
-            );
+            const responseData = patientsResponse.data.data;
+            const content = responseData?.data ?? [];
+            const transformedPatients = transformPatientsData(content);
 
             setDoctorPatients(transformedPatients || []);
           } else {
@@ -198,6 +204,9 @@ const DoctorDashaboard = () => {
 
     fetchGraphData();
   }, [selectedTimePeriod]);
+
+
+  console.log(doctorOrders,'doc orders');
 
   // Get dynamic counts from API data
   const statsCounts = getStatsCounts(doctorStats.data);
@@ -323,7 +332,7 @@ const DoctorDashaboard = () => {
             doctorOrders && (
               <TableComponent
                 headings={headingsOrder}
-                data={doctorOrders.length > 0 ? doctorOrders : dataOrder}
+                data={doctorOrders.length > 0 ? doctorOrders :[]}
                 actionHrefKey="detailUrl"
               />
             )
