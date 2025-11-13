a146e9f06c60debebbcb57dec9446d7e76db47ea
Sharjeel <sharjeel.nust07@gmail.com>
2025-11-13 17:12:53 +0500

    updated design and functionality of Add Product

---
+++ src/icon/UploadIcon.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+function UploadIcon() {
+  return (
+    <div>
+      <svg
+        width="95"
+        height="95"
+        viewBox="0 0 95 95"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clipPath="url(#clip0_14178_12766)">
+          <rect
+            width="95"
+            height="95"
+            rx="47.5"
+            fill="#EF6A1F"
+            fillOpacity="0.05"
+          />
+          <circle cx="47.5" cy="47.5" r="47.5" fill="#F8F8F8" />
+          <path
+            d="M59.1245 51.3748V56.5414C59.1245 57.2266 58.8523 57.8836 58.3679 58.3681C57.8834 58.8526 57.2263 59.1248 56.5412 59.1248H38.4578C37.7727 59.1248 37.1156 58.8526 36.6312 58.3681C36.1467 57.8836 35.8745 57.2266 35.8745 56.5414V51.3748"
+            stroke="#001D58"
+            strokeWidth="2.58333"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M53.9596 42.3328L47.5013 35.8745L41.043 42.3328"
+            stroke="#001D58"
+            strokeWidth="2.58333"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M47.5 35.8745V51.3745"
+            stroke="#001D58"
+            strokeWidth="2.58333"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_14178_12766">
+            <rect width="95" height="95" rx="47.5" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+export default UploadIcon;
---
+++ src/modals/EditBrandsModal.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function EditBrandsModal({ isOpen, onClose, onSave }) {
+  const [categories, setCategories] = useState([
+    "Brand A",
+    "Brand A",
+    "Brand A",
+    "Brand A",
+    "Brand A",
+    "Brand A",
+  ]);
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+      <div className="bg-bgWhite rounded-2xl shadow-xl w-full max-w-md p-6 animate-fadeIn">
+        <h2 className="text-xl font-bold font-poppins text-[#434343] mb-5">
+          Edit Brand
+        </h2>
+
+        <div className="space-y-3 border border-[#E5E5E5] p-4 rounded-2xl">
+          {categories.map((cat, i) => (
+            <input
+              key={i}
+              //   value={cat}
+              //   onChange={(e) => handleChange(i, e.target.value)}
+              className="w-full border   border-[#E5E5E5] rounded-xl px-4 py-3 placeholder:text-gray-400 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-200"
+              placeholder="Brand A"
+            />
+          ))}
+        </div>
+
+        <div className="flex justify-between gap-3 mt-8 ">
+          <button
+            onClick={() => onClose()}
+            className="px-[72px] py-3 font-poppins rounded-full bg-[#F8F8F8] text-[#434343] text-sm font-semibold hover:bg-gray-200 transition"
+          >
+            Cancel
+          </button>
+
+          <button className="px-[72px] py-3 font-poppins rounded-full bg-[#001F54] text-white text-sm font-semibold  hover:bg-[#002b75] transition">
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
